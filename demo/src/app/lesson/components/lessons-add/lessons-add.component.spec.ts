a5fd131a33d12eff5dc5dd6cb68521954b8ffdd8
tati oliveira <tat1roliv@gmail.com>
2023-03-09 17:30:00 -0300

    Update lessons-add.component.spec.ts

--- demo/src/app/lesson/components/lessons-add/lessons-add.component.spec.ts
+++ demo/src/app/lesson/components/lessons-add/lessons-add.component.spec.ts
@@ -2,13 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LessonsAddComponent } from './lessons-add.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/material.module';
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('LessonsAddComponent', () => {
   let component: LessonsAddComponent;
   let fixture: ComponentFixture<LessonsAddComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LessonsAddComponent ]
+      declarations: [ LessonsAddComponent ],
+      imports: [
+        ReactiveFormsModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
 
