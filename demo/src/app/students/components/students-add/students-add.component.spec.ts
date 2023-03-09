343e3a8c4be7ef63bd551c21750459e0270bccd8
tati oliveira <tat1roliv@gmail.com>
2023-03-09 17:28:53 -0300

    Update students-add.component.spec.ts

--- demo/src/app/students/components/students-add/students-add.component.spec.ts
+++ demo/src/app/students/components/students-add/students-add.component.spec.ts
@@ -2,13 +2,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StudentsAddComponent } from './students-add.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/material.module';
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+
 describe('StudentsAddComponent', () => {
   let component: StudentsAddComponent;
   let fixture: ComponentFixture<StudentsAddComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StudentsAddComponent ]
+      declarations: [ StudentsAddComponent ],
+      imports: [
+        ReactiveFormsModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
 
